9a25292914ff8f225ad4a6dc63ebe212c10b608f
raknage <raknage@raknage.com>
2025-06-11 10:55:13 +0300

    verbose

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    messages = [types.Content(role="user", parts=types.Part(text=prompt))]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     model = "gemini-2.0-flash-001"
     response = client.models.generate_content(model=model, contents=messages)
 
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 main()
