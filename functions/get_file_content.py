2a6edc1316dbea601dad8c7f6b8ae3a489920640
raknage <raknage@raknage.com>
2025-06-11 22:52:16 +0300

    get file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+
+MAX_CHARS = 10000
+
+
+def get_file_content(working_directory, file):
+    cwd_abspath = os.path.abspath(working_directory)
+    file_abspath = os.path.abspath(os.path.join(working_directory, file))
+    if not os.path.isfile(file_abspath):
+        return f'Error: File not found or is not a regular file: "{file}"'
+    if not file_abspath.startswith(cwd_abspath):
+        return f'Error: Cannot read "{file}" as it is outside the permitted working directory'
+
+    with open(file_abspath, "r") as f:
+        file_content_string = f.read(MAX_CHARS)
+    if len(file_content_string) >= MAX_CHARS:
+        file_content_string += f'[...File "{file}" truncated at {MAX_CHARS} characters]'
+
+    return file_content_string
--- tests.py
+++ tests.py
@@ -1,6 +1,12 @@
-from functions.get_file_info import get_file_info
+# from functions.get_file_info import get_file_info
+from functions.get_file_content import get_file_content
 
-print(get_file_info("calculator", "."))
-print(get_file_info("calculator", "pkg"))
-print(get_file_info("calculator", "/bin"))
-print(get_file_info("calculator", "../"))
+# print(get_file_info("calculator", "."))
+# print(get_file_info("calculator", "pkg"))
+# print(get_file_info("calculator", "/bin"))
+# print(get_file_info("calculator", "../"))
+
+# print(get_file_content("calculator", "lorem.txt"))
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
