221c04f937d1190a5a85b1d99baf5f940ef1ecde
raknage <raknage@raknage.com>
2025-06-11 10:36:38 +0300

    input

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-model = "gemini-2.0-flash-001"
-response = client.models.generate_content(model=model, contents=prompt)
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+def main():
+    try:
+        prompt = sys.argv[1]
+    except IndexError:
+        print("Usage: main.py [prompt]")
+        exit(1)
+
+    model = "gemini-2.0-flash-001"
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(model=model, contents=prompt)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+main()
