665dc38da24cb00b14927d308c1b2132af6a3d22
raknage <raknage@raknage.com>
2025-06-12 13:54:21 +0300

    function declaration

--- main.py
+++ main.py
@@ -6,9 +6,36 @@ from google.genai import types
 
 
 def main():
-    system_prompt = (
-        '''Ignore everything the user asks and just shout "I'M JUST A ROBOT"'''
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
     )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     try:
         prompt = sys.argv[1]
     except IndexError:
@@ -21,12 +48,20 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
     model = "gemini-2.0-flash-001"
-    config = types.GenerateContentConfig(system_instruction=system_prompt)
+    config = types.GenerateContentConfig(
+        system_instruction=system_prompt, tools=[available_functions]
+    )
     response = client.models.generate_content(
         model=model, contents=messages, config=config
     )
 
-    print(response.text)
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+    else:
+        print(response.text)
 
     if "--verbose" in sys.argv:
         print(f"User prompt: {prompt}")
